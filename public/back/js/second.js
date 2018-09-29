6303461a40ab4934e909ba84fcb3025a2c198332
bello <bello96@163.com>
2018-09-29 22:59:35 +0800

    二级添加分类模态框内容完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -1,5 +1,5 @@
 $(function(){
-    //一进入页面就发送ajax请求,获取后台数据,通过模板引擎渲染到页面上
+    //1.一进入页面就发送ajax请求,获取后台数据,通过模板引擎渲染到页面上
     var currentPage = 1;  //当前页
     var pageSize = 5;  //每页显示的条数
     rander();
@@ -13,7 +13,7 @@ $(function(){
                 pageSize: pageSize
             },
             success:function(info){
-                console.log(info);
+                // console.log(info);
                 var str = template("tmp",info);
                 $("tbody").html(str);
     
@@ -31,5 +31,48 @@ $(function(){
             }
         });
     }
+
+    //2. 点击添加分类按钮,显示模态框
+    $("#addBtn").click(function(){
+        $("#addModal").modal("show");
+
+        //发送ajax请求,请求所有的一级分类 进行列表渲染
+        //利用分页接口 获取全部的一级分类的额数据,传page = 1,pageSize = 100,进行模拟
+        $.ajax({
+            type:"get",
+            url:"/category/queryTopCategoryPaging",
+            dataType:"json",
+            data:{
+                page:1,
+                pageSize:100
+            },
+            success:function(info){
+                console.log(info)
+                var str = template("tmp2",info);
+                $(".dropdown-menu").html(str);
+            }
+        })
+    })
+
+    //3. 给下拉列表中的,每个a添加点击事件,获取a的文本,设置给按钮
+    $(".dropdown-menu").on("click","a",function(){
+        // console.log(this);  this就是a
+        //获取文本
+        var txt = $(this).text();
+        //设置给按钮
+        $("#dropdownTxt").text(txt);
+    })
+
+    //4. 文件上传初始化
+    $("#fileupload").fileupload({
+        dataType:"json",
+        done:function (e, data) {
+            // console.log(data.result);
+            //获取后台返回的图片地址
+            var picUrl = data.result.picAddr;
+            // console.log(picUrl)
+            $("#imgBox img").attr("src",picUrl);
+        }
+    })
 });
 
