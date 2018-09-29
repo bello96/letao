a720a571b9857d075350cb5ab86dcf8d851f4fd5
bello <bello96@163.com>
2018-09-29 20:51:58 +0800

    一级分类添加完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -44,4 +44,56 @@ $(function(){
     $("#addBtn").click(function(){
         $("#addModal").modal("show");
     })
+
+
+    //通过表单校验插件,实现表单校验功能
+    $("#form").bootstrapValidator({
+        //配置表单的对号和差好的图标
+        //固定的写法
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',    //校验成功
+            invalid: 'glyphicon glyphicon-remove',   //校验失败
+            validating: 'glyphicon glyphicon-refresh'  //校验中
+        },
+        //配置校验字段(这里就自由)
+        fields: {
+            categoryName: {
+                //配置校验规则
+                validators: {
+                    //非空检验
+                    notEmpty: {
+                        message:"请输入一级分类名称"
+                    }
+                }
+            }
+        }
+    })
+
+    //阻止校验成功时的默认提交,需要通过ajax提交
+    //这里需要注册表单校验成功事件
+    $("#form").on("success.form.bv",function(e){
+        //阻止默认事件
+        e.preventDefault();
+
+        //通过ajax提交
+        $.ajax({
+            url:"/category/addTopCategory",
+            type:"post",
+            dataType:"json",
+            data:$("#form").serialize(),
+            success:function(info){
+                // console.log(info)
+                if(info.success){
+                    //关闭模态框
+                    $("#addModal").modal("hide");    
+                    //重新渲染
+                    rander();
+                    //表单内容和校验的状态都需要重置,resetForm()里面穿true,会将表单的状态和内容都重置
+                    $("#form").data("bootstrapValidator").resetForm(true);    
+                }
+            }
+                    
+        })
+    })
+
 })
