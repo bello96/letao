e20a01f1b1508da28fb27baf0673cf37d124c509
bello <bello96@163.com>
2018-10-11 14:44:41 +0800

    购物车商品删除-编辑完成

--- public/front2/js/cart.js
+++ public/front2/js/cart.js
@@ -1,7 +1,7 @@
 // 购物车
 $(function(){
 
-    //已进入页面,请求当前登录的用户的所有的购物车信息
+    //1.已进入页面,请求当前登录的用户的所有的购物车信息
     //发送请求
         //1.用户未登录,需要拦截到登录页
         //2.用户登录,进行购物车的渲染
@@ -42,6 +42,79 @@ $(function(){
             } 
           }
         }
-      })
+      });
+
+    
+    //2.左滑删除功能
+      //(1),给删除按钮绑定点击事件,通过事件委托,且需要用tap
+      //(2),获取当前购物车的id
+      //(3),发送ajax请求进行删除
+      //(3),页面重新渲染
+    
+    $(".lt_main").on("tap",".btn_delete",function(){
+        //获取id
+        var id = $(this).data("id");
+
+        //发送ajax请求
+        $.ajax({
+            type:"get",
+            url:"/cart/deleteCart",
+            dataType:"json",
+            data:{id:[id]},
+            success:function(info){
+                // console.log(info)
+                if(info.success){
+                    // 页面重新渲染, 触发一次下拉刷新即可
+                    mui(".mui-scroll-wrapper").pullRefresh().pulldownLoading();
+                }  
+            }
+        })
+    });
+
+    //3.编辑功能
+    //点击编辑按钮,显示确认框(通过事件委托)
+    $(".lt_main").on("tap",".btn_edit",function(){
+        //自定义属性dataset,dom对象的属性
+        var obj = this.dataset;
+        //从自定义的属性中获取id
+        var id = obj.id;
+        var htmlStr = template("editTpl",obj);
+        htmlStr = htmlStr.replace( /\n/g, "" );
+
+        //显示确认框
+        mui.confirm(htmlStr, "编辑商品", ["确认", "取消"],function(e){
+            if(e.index === 0) {
+                //确认编辑
+                //获取尺码和数量,进行提交
+                var size = $('.lt_size span.current').text();
+                var num = $('.mui-numbox-input').val();
+
+                $.ajax({
+                    url:"/cart/updateCart",
+                    type:"post",
+                    dataType:"json",
+                    data:{
+                        id:id,
+                        size:size,
+                        num:num
+                    },
+                    success:function(info){
+                        console.log(info);
+                        if(info.success){
+                            // 编辑成功, 页面重新, 下拉刷新一次即可
+                         mui(".mui-scroll-wrapper").pullRefresh().pulldownLoading();
+                        }
+                    }
+                })
+            }
+        });
+        // 手动初始化数字框
+        mui(".mui-numbox").numbox();
+    })
+
+    // 给编辑模态框的尺码添加选中功能
+  $('body').on("click", ".lt_size span", function() {
+    $(this).addClass("current").siblings().removeClass("current");
+  });
 
 })
